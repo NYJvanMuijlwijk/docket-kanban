722febf912a270a887a91c06612f07e324149b10
DaemonInc <DaemonInc@users.noreply.github.com>
2026-03-15 12:35:24 +0100

    logout button

--- app/lib/features/boards/screens/board_list_screen.dart
+++ app/lib/features/boards/screens/board_list_screen.dart
@@ -1,15 +1,25 @@
+import 'package:app/features/auth/providers/firebase_auth_service_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Screen that displays a list of Kanban boards available to the user.
-class BoardListScreen extends StatelessWidget {
+class BoardListScreen extends ConsumerWidget {
   /// Creates a new instance of [BoardListScreen].
   const BoardListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Boards'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await ref.read(firebaseAuthServiceProvider).signOut();
+            },
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Board List Content Goes Here'),
